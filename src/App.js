b90593e46b733956d9eb6eb681d47643d02feb7a
gyuuuu <kai9702@gmail.com>
2021-06-01 00:41:46 +0900

    get movie data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component{
     state = {
@@ -7,10 +8,12 @@ class App extends React.Component{
         movies: []
     };
 
-    componentDidMount(){
-        setTimeout(() => {
-            this.setState({ isLoading: false })
-        }, 6000)
+    getMovies = async () => {
+        const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+    }
+
+    async componentDidMount(){
+        this.getMovies();
     }
 
     render(){
