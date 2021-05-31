1b664bba9d44ba0084729148d29c8342040e0796
gyuuuu <kai9702@gmail.com>
2021-06-01 00:30:30 +0900

    componentDidMount

--- src/App.js
+++ src/App.js
@@ -3,23 +3,21 @@ import React from "react";
 
 class App extends React.Component{
     state = {
-        count: 0
+        isLoading: true,
+        movies: []
     };
 
-    add = () => {
-        this.setState(current => ({ count: current.count + 1 }))
-    };
-    minus = () => {
-        this.setState(current => ({ count: current.count - 1 }))
-    };
+    componentDidMount(){
+        setTimeout(() => {
+            this.setState({ isLoading: false })
+        }, 6000)
+    }
 
     render(){
+        const { isLoading } = this.state;
         return (
             <div>
-                <h1>The number is: {this.state.count}</h1>
-                {/* add()이렇게 하지 않고 add이렇게만 -> 클릭할때만 호출 */}
-                <button onClick={this.add}>Add</button> 
-                <button onClick={this.minus}>Minus</button>
+                {isLoading ? "Loading..." : "We are ready"}
             </div>
         );
     }
